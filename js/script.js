c2b78dd2b0bb631d498fbbef0887335746b8b643
BeAzarym <58728526+BeAzarym@users.noreply.github.com>
2020-08-22 14:27:20 +0200

    Add files via upload
    Ajout Image, lien, calcul

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
 // Projet Août
 "use strict";
-let poche = [{bookName: "Replay", bookAuthor: "Ken Grimwood", bookPrice: "7.99", bookYear: "1997", bookStyle: "Aventures", bookStatus: false }];
-let bibliotheque = [{bookName: "Les 4 accords de Toltèques", bookAuthor: "Miguel Ruiz", bookPrice: "49,55", bookYear: "1985", bookStyle: "Encyclopédie",bookStatus: true},
-                    {bookName: "Cherub", bookAuthor: "Robert Muchamore", bookPrice: "29,99", bookYear: "2016", bookStyle: "Aventures", bookStatus: true},
-                    {bookName: "Replay", bookAuthor: "Ken Grimwood", bookPrice: "7.99", bookYear: "1997", bookStyle: "Aventures", bookStatus: false }];
+let poche = [{Nom: "Replay", Auteur: "Ken Grimwood", Prix: 7.99, Annee: "1997", Genre: "Aventures", Statut: false }];
+let bibliotheque = [{Nom: "Les 4 accords de Toltèques", Auteur: "Miguel Ruiz", Prix: 49.55, Annee: "1985", Genre: "Encyclopédie",Statut: true},
+                    {Nom: "Cherub", Auteur: "Robert Muchamore", Prix: 29.99, Annee: "2016", Genre: "Aventures", Statut: true},
+                    {Nom: "Replay", Auteur: "Ken Grimwood", Prix: 7.99, Annee: "1997", Genre: "Aventures", Statut: false }];
 function init() {
     document.getElementById("collectionLivre").innerHTML += genererTable(bibliotheque);
     document.getElementById("pocheLivre").innerHTML += genererTable(poche);
+    sommeLivre(poche);
 }
 function sauvegardeDonne () {
     let bookName = document.getElementById("bookName").value;
@@ -17,12 +18,12 @@ function sauvegardeDonne () {
     let bookStatus = true;
 
     function book(bookName, bookAuthor, bookPrice, bookYear, bookStyle, bookStatus) {
-        this.bookName = bookName;
-        this.bookAuthor = bookAuthor;
-        this.bookPrice = bookPrice;
-        this.bookYear = bookYear;
-        this.bookStyle = bookStyle;
-        this.bookStatus = bookStatus
+        this.Nom = bookName;
+        this.Auteur = bookAuthor;
+        this.Prix = bookPrice;
+        this.Annee = bookYear;
+        this.Genre = bookStyle;
+        this.Statut = bookStatus
     }
 
     let bookCreator = new book (bookName, bookAuthor, bookPrice, bookYear, bookStyle, bookStatus);
@@ -31,18 +32,19 @@ function sauvegardeDonne () {
         bibliotheque.push(bookCreator);
         console.log(bibliotheque);
         document.getElementById("collectionLivre").innerHTML = genererTable(bibliotheque);
-    } else {alert("Le livre " + bookCreator.bookName + " est déja présent dans la bibliothèque.");}
+    } else {alert("Le livre " + bookCreator.Nom + " est déja présent dans la bibliothèque.");}
 }
 
 function emprunter(){
     let name = document.getElementById("myBookName").value;
     for (let i = 0;i < bibliotheque.length;i++){
-        if(bibliotheque[i].bookName === name){
-           bibliotheque[i].bookStatus = false;
+        if(bibliotheque[i].Nom === name){
+           bibliotheque[i].Statut = false;
            let livre = bibliotheque[i];
            poche.push(livre);
-            document.getElementById("collectionLivre").innerHTML = genererTable(bibliotheque);
-            document.getElementById("pocheLivre").innerHTML = genererTable(poche);
+           document.getElementById("collectionLivre").innerHTML = genererTable(bibliotheque);
+           document.getElementById("pocheLivre").innerHTML = genererTable(poche);
+           sommeLivre(poche);
         }
     }
 }
@@ -50,23 +52,32 @@ function emprunter(){
 function rendre() {
     let name = document.getElementById("myBookName").value;
     for (let i = 0;i < poche.length;i++){
-        if(poche[i].bookName === name){
+        if(poche[i].Nom === name){
             poche[i] = {};
         }
-        if(bibliotheque[i].bookName === name){
-            bibliotheque[i].bookStatus = true;
+    }
+    for (let i = 0;i < bibliotheque.length;i++){
+        if(bibliotheque[i].Nom === name){
+            bibliotheque[i].Statut = true;
             document.getElementById("collectionLivre").innerHTML = genererTable(bibliotheque);
             document.getElementById("pocheLivre").innerHTML = genererTable(poche);
-    }
 
+        }
     }
 }
 
-
 function checkData(newBook) {
     let t = bibliotheque.filter(function (x) {
-            return (x.bookName === newBook.bookName);});
+            return (x.Nom === newBook.Nom);});
     return t.length !== 0;
+}
+function sommeLivre(tableau){
+    if(tableau.length === 0) return "0";
+    let resultat = 0;
+    for (let i = 0; i < tableau.length;i++){
+         resultat += parseFloat(tableau[i].Prix);
+    }
+    document.getElementById("pocheValeur").innerHTML= "La valeur total de vos livres est de " +resultat.toFixed(2) + "€";
 }
 
 function test (){
@@ -74,7 +85,7 @@ function test (){
     document.getElementById("bookAuthor").value = "Jk Rowling";
     document.getElementById("bookPrice").value = 63.99;
     document.getElementById("bookYear").value = 1991;
-    document.getElementById("bookStyle").value = "Romancier"
+    document.getElementById("bookStyle").value = "Aventures"
 }
 
 function genererTable(tableauObjets){
